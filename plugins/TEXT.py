6b9fdf419925c3332bb9f3ed288f45b1ff359403
pctechkid <r0nnelomb0y@gmail.com>
2019-12-24 00:58:48 +0800

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -1,7 +1,7 @@
 
 
 # Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
-drive_folder_name = "GDriveUploaderBot"
+drive_folder_name = "Remote Uploads"
 
 
 # Enter Your Mega email And Pass (Required)
@@ -9,7 +9,7 @@ MEGA_EMAIL = "bearyan8@yandex.com"
 MEGA_PASSWORD = "bearyan8@yandex.com"
 
 
-START = " Hi {}  \nI am Drive Uploader Bot . Please Authorise To use me .By using /auth \n\n For more info /help \n\n Third-Party Website \n Support Added /update \n\n For Bot Updates  \n <a href ='https://t.me/aryan_bots'>Join Channel</a>\nPlease Report Bugs  @aryanvikash"
+START = " Hi {}  \nI am Google Drive Uploader made by Ronnel. Please Authorize me to use your google drive. Use the /auth command to proceed!"
 
 HELP = """   <b>AUTHORISE BOT</b> 
        Use  /auth Command Generate
@@ -29,10 +29,6 @@ HELP = """   <b>AUTHORISE BOT</b>
               500 KBps :(   ]
             * Dropbox links 
             *  Mega links
-            
-            + More On Its way:)
-                
-Bug Report @aryanvikash
         """
 DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
 OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n Openload Links Are Extremely Slow"
@@ -40,18 +36,18 @@ PROCESSING = "Processing Your Request ...!!"
 DOWN_TWO = True
 DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started... \n  Mega Links are \n Extremely Slow :("
-DOWN_COMPLETE = "Downloading complete !!"
-NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth  \n\n @aryanvikash"
-REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @aryanvikash "
-AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
+DOWN_COMPLETE = "Downloading complete!"
+NOT_AUTH = "Sorry. You have not authorized me yet to use your drive. \n\n Please Authorise Me Using /auth"
+REVOKE_FAIL = "You have now removed authorization from your Google Drive \n. Please use /auth to authorize again!"
+AUTH_SUCC = "Authorized Successfully! \n\n You can now start uploading files by sending your links :)"
 ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\n report At @aryanvikash "
-AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
+AUTH_URL = '<a href ="{}">Click this Url</a> \n Generate and copy the generated Google Drive token then send it here.'
 UPLOADING = "Download Complete !! \n Uploading Your file"
-REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
+REVOKE_TOK = " Your Token is Revoked Successfully! \n\n Use /auth To Re-Authorise Your Drive Acc. "
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
-DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me  \n\n report At @aryanvikash"
+DOWNLOAD_URL = "Your File Uploaded Successfully! \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
+AUTH_ERROR = "AUTH Error! Please  Send Me a  valid Token or Re-authenticate me!"
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
